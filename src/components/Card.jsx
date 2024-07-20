b0095a02734e5d14f4a2efd1d153a3f38ab22abf
Justin Klitgaard <jlke45@gmial.com>
2024-07-20 13:58:12 +0900

    Update card quantity logic
    If the user selects two of the same item, rather than being added
    seperately to the cart, the quantity of a kind of item is calculated in
    the cart. E.g., rather than "Hat, quantity 1" appearing twice in the
    cart, "Hat, quantity 2" appears.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,8 +5,20 @@ import { nanoid } from "nanoid";
 const Card = (props) => {
   const [cart, setCart] = useContext(CartContext);
 
-  const handleClick = (item) => {
-    const newItem = { ...item, quantity: 1, id: nanoid() };
+  const handleClick = (addedItem) => {
+    const newItem = { ...addedItem, quantity: 1, id: nanoid() };
+    if (cart.some((item) => item.name === newItem.name)) {
+      const updatedCart = cart.map((item) => {
+        if (item.name === newItem.name) {
+          const newQuantity = item.quantity + 1;
+          return { ...item, quantity: newQuantity };
+        } else {
+          return item;
+        }
+      });
+      setCart(updatedCart);
+      return;
+    }
     setCart(cart.concat(newItem));
   };
 
